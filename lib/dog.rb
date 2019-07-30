0add47c20d6a2240085163ded8084514ef20bbd7
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-30 13:21:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,6 @@
 class Dog 
- def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name
-    @this_dogs_name
-  end
-end
+ def name =(fido)
+   
  
 lassie = Dog.new
 lassie.name = "Lassie"
